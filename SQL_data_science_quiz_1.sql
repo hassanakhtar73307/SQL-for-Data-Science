1f841c55a76ce23c8561c1b8e112b1b9ea039ce2
Hassan Akhtar <69019277+hassanakhtar73307@users.noreply.github.com>
2022-11-14 03:10:02 +0500

    Update SQL_data_science_quiz_1.sql
    Hey my name is hassan akhtar actually tomorrow is my lab exam can you answer these queries ?
    
    1. Employee Michael will report to Nancy from now onwards. Update the record accordingly. 
    2. Select the tracks where the composer fields are null.
    3. Delete at least 3 artistids who don’t have any album. 
    4. Provide a query showing Customers (just their full names, customer ID and country) who are not     in the US.
    5. Provide a query showing only the Employees who are Sales Agents.
    6. Select the tracks where the composer is ‘Foo Fighters’ and the track length is greater than 300000 milliseconds in order of trackid.
    7. Find the Artists that have the word ‘Orchestra’ in the name.

--- SQL_data_science_quiz_1.sql
+++ SQL_data_science_quiz_1.sql
@@ -17,3 +17,12 @@ WHERE BirthDate = '1965-03-03 00:00:00'
 
 select * from Tracks
 limit 20
+can you answer these SQL3 chinook queries ?
+
+Employee Michael will report to Nancy from now onwards. Update the record accordingly. 
+2. Select the tracks where the composer fields are null.
+3. Delete at least 3 artistids who don’t have any album. 
+4. Provide a query showing Customers (just their full names, customer ID and country) who are not     in the US.
+5. Provide a query showing only the Employees who are Sales Agents.
+6. Select the tracks where the composer is ‘Foo Fighters’ and the track length is greater than 300000 milliseconds in order of trackid.
+7. Find the Artists that have the word ‘Orchestra’ in the name.
